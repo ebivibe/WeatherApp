43682231d09a3dbc272a4ad1087e91fc99e4bb5e
ebivibe <ivanabe3@gmail.com>
2019-02-09 09:14:18 -0800

    more set up

--- parsing.js
+++ parsing.js
@@ -1,3 +1,20 @@
-function Generate(){
-    alert("test");
+function Generate(lat, lon, form){
+    alert("working");
+    form.coordinates.value="("+lat+", "+lon+")";
+            var request = new XMLHttpRequest();
+            // Open a new connection, using the GET request on the URL endpoint
+            var x='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID='+keynums;
+            request.open('GET', x , true);
+            request.onload = function () {
+                
+                var data = JSON.parse(this.response);
+                
+                form.location.value=data.name;
+
+                form.temp.value=String(parseFloat(data.main.temp)-273.15);
+    
+                form.forecast.value=data.weather[0].description;
+                
+            }
+            request.send();
 }
